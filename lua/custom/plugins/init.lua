b1e409e387ddc258270bb7477f79dd823b63f580
Catalin Miu <cmi007@gmail.com>
2024-10-10 13:45:50 +0300

    update config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,20 @@
 --
 -- See the kickstart.nvim README for more information
 
+local function copy()
+  if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
+    require('osc52').copy_register '"'
+  end
+end
+
+vim.api.nvim_create_autocmd('TextYankPost', { callback = copy })
+
 return {
   'farmergreg/vim-lastplace',
+  {
+    'ojroques/nvim-osc52',
+    config = function()
+      require('osc52').setup {}
+    end,
+  },
 }
